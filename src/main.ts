6b76988340b98ff6f664b44df2e3654db8792ec5
HaoWang95 <alanwang0028@gmail.com>
2021-08-06 12:26:54 +1000

    swagger documentation set up

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+// module declaration
 declare const module: any;
 
-// hot-reload
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Basic swagger document configuration
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs Todo App Api Documentation')
+    .setDescription('The todo api description')
+    .setVersion('1.0')
+    .addTag('Alan')
+    .build()
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 
+  // Development hot-reload setup using webpack-hmr.config.js
   if(module.hot){
     module.hot.accept();
     module.hot.dispose(() => app.close());
