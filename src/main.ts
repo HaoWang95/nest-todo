2a44bd463105029f7b6dd3c9a575efa21cfbaf52
HaoWang95 <alanwang0028@gmail.com>
2021-08-06 16:34:33 +1000

    Update: dev dependencies for graphql

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .setTitle('Nestjs Todo App Api Documentation')
     .setDescription('The todo api description')
     .setVersion('1.0')
-    .addTag('Alan')
+    .addTag('ToDo')
     .build()
   
   const document = SwaggerModule.createDocument(app, config);
